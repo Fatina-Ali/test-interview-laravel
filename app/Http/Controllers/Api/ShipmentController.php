2a053b5fd427afa2b6a679eb30d8b6620d0ce149
fatenaali09 <fatenaali09@gmail.com>
2025-01-20 08:55:57 +0200

    Add address limit check and geolocation API call to shipment controller

--- app/Http/Controllers/Api/ShipmentController.php
+++ app/Http/Controllers/Api/ShipmentController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\AddressResource;
+use App\Models\Country;
 use App\Http\Resources\ShipmentResource;
 use App\Models\Address;
 use App\Models\Client;
 use App\Models\Shipment;
-use App\Models\ShipmentImage;
+use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Client as HttpClient;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -40,79 +41,48 @@ class ShipmentController extends Controller
      */
     public function store(Request $request)
     {
-       try{
-            $request->validate([
+        try {
+            $validated = $request->validate([
                 'description'   => 'required',
                 'receiver'  => 'required',
                 'sender_address'    => 'required',
                 'receiver_address'  => 'required',
-                'created_at_unix'   => 'required|integer'
+                'created_at_unix'   => 'required|integer',
             ]);
-            $serialNum =  Shipment::getServiceNum();
 
-            if(empty($request->receiver['id'])){
-                $receiver = Client::create([
-                    'phone' => $request->receiver['phone'],
-                    'name'  => $request->receiver['name'],
-                    'image' => 'uploads/images/user_default_image.png'
-                ]);
-            }else{
-                $receiver = Client::findOrFail($request->receiver['id']);
-            }
-            if(empty($request->receiver_address['id'])){
-                $receiverCountry = \App\Models\Country::find($request->receiver_address['country']['id']);
-                $this->checkCountryLimitations($receiverCountry);
-                $receiverAddress = Address::create([
-                    'name' => $request->receiver_address['name'],
-                    'latitude'  => $request->receiver_address['latitude'],
-                    'longitude'  => $request->receiver_address['longitude'],
-                    'country_id'    => $request->receiver_address['country']['id'],
-                    'client_id' => $receiver->id
-                ]);
-                $receiverAddress->setRelation('country',$receiverCountry);
-            }else{
-                $receiverAddress = Address::findOrFail($request->receiver_address['id']);
-                $this->checkCountryLimitations($receiverAddress->country);
-            }
-            $sender = Client::where('user_id',$this->user_id)->first();
-            if(empty($request->sender_address['id'])){
-                $senderCountry = \App\Models\Country::find($request->sender_address['country']['id']);
-                $senderAddress = Address::create([
-                    'name' => $request->sender_address['name'],
-                    'latitude'  => $request->sender_address['latitude'],
-                    'longitude'  => $request->sender_address['longitude'],
-                    'country_id'    => $request->sender_address['country']['id'],
-                    'client_id' => $sender->id
-                ]);
-                $senderAddress->setRelation('country',$senderCountry);
-            }else{
-                $senderAddress = Address::findOrFail($request->receiver_address['id']);
-            }
+            DB::beginTransaction();
 
+            $serialNum = Shipment::getServiceNum();
+
+            // Process receiver
+            $receiver = $this->processClient($request->receiver);
+            // Process receiver address
+            $receiverAddress = $this->processAddress($request->receiver_address, $receiver);
+
+            // Process sender
+            $sender = Client::where('user_id', $this->user_id)->first();
+            // Process sender address
+            $senderAddress = $this->processAddress($request->sender_address, $sender);
+
+            // Create shipment
             $shipment = Shipment::create([
-                'serial_num'    => $serialNum,
-                'sender_id'       => $sender->id,
-                'description'       => $request->description ?? '',
-                'receiver_id'       => $receiver->id,
+                'serial_num' => $serialNum,
+                'sender_id' => $sender->id,
+                'description' => $validated['description'],
+                'receiver_id' => $receiver->id,
                 'sender_address_id' => $senderAddress->id,
-                'receiver_address_id'  => $receiverAddress->id,
-                'created_at_unix'   => $request->created_at_unix,
-                'status'            => 1
+                'receiver_address_id' => $receiverAddress->id,
+                'created_at_unix' => $validated['created_at_unix'],
+                'status' => 1,
             ]);
 
-            $shipment->setRelation('receiver',$receiver);
-            $shipment->setRelation('sender',$sender);
-            $shipment->setRelation('receiverAddress',$receiverAddress);
-            $shipment->setRelation('senderAddress',$senderAddress);
-            return apiResponse(['shipment'  => new ShipmentResource($shipment)]);
-       }catch(Exception $e){
-            return \apiResponse(null,$e->getMessage(),400);
-       }
+            DB::commit();
 
-
-
-
-
+            return apiResponse(['shipment' => new ShipmentResource($shipment)]);
+        } catch (Exception $e) {
+            DB::rollBack();
+            return apiResponse(null, $e->getMessage(), 400);
+        }
     }
 
     /**
@@ -172,7 +142,60 @@ class ShipmentController extends Controller
         return  apiResponse(null,__('Deleted Successfully'));
     }
 
+    private function processClient($data)
+    {
+        if (empty($data['id'])) {
+            return Client::create([
+                'phone' => $data['phone'],
+                'name' => $data['name'],
+                'image' => 'uploads/images/user_default_image.png',
+            ]);
+        }
 
+        return Client::findOrFail($data['id']);
+    }
+
+    private function processAddress($data, $client)
+    {
+        if (empty($data['id'])) {
+            $country = Country::findOrFail($data['country']['id']);
+            $this->checkCountryLimitations($country);
+
+            if (Address::where('client_id', $client->id)->count() >= 100) {
+                throw new Exception("You've reached out the maximum count of addresses!");
+            }
+
+            $formatted_address = $this->getFormattedAddress($data['latitude'], $data['longitude']);
+
+            $address = Address::create([
+                'name' => $data['name'],
+                'latitude' => $data['latitude'],
+                'longitude' => $data['longitude'],
+                'location_description' => $formatted_address,
+                'country_id' => $data['country']['id'],
+                'client_id' => $client->id,
+            ]);
+
+            $address->indexByLocation();
+            $address->setRelation('country', $country);
+
+            return $address;
+        }
+
+        return Address::findOrFail($data['id']);
+    }
+
+    private function getFormattedAddress($latitude, $longitude)
+    {
+        $apiKey = env('GOOGLE_MAPS_API_KEY');
+        $url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={$latitude},{$longitude}&key={$apiKey}";
+
+        $client = new HttpClient();
+        $response = $client->get($url);
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        return $data['results'][0]['formatted_address'] ?? null;
+    }
     private function checkCountryLimitations($country){
         if(Shipment::whereHas('receiverAddress',function($query) use($country){
             $query->where('country_id',$country->id);
